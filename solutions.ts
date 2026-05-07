3c0fd4feb0fc90aa5cbfdfc1ba2577ddd6f325fc
imran hossain <imranme.global@gmail.com>
2026-05-07 17:59:36 +0600

    finally done

--- solutions.ts
+++ solutions.ts
@@ -4,8 +4,6 @@ const filterEvenNumbers = (numbers: number[]): number[] => {
     return numbers.filter(num => num % 2 === 0);
 };
 
-// const result = filterEvenNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
-// console.log(result); 
 
 
 
@@ -15,8 +13,6 @@ const reverseString = (strings: string): string => {
     return strings.split('').reverse().join('');
 };
 
-// const result = reverseString('hello')
-// console.log(result)
 
 
 
@@ -28,8 +24,6 @@ const checkType = (value: StringOrNumber): StringOrNumber => {
     return typeof value === "string" ? "String" : "Number";
 }
 
-// console.log(checkType("hello"));
-// console.log(checkType(42)); 
 
 
 // Problem 4:
@@ -38,10 +32,7 @@ const getProperty = <T, K extends keyof T>(obj: T, key: K): T[K] => {
     return obj[key];
 };
 
-const user1 = { id: 1, name: "imran", age: 21 };
-const user2 = { id: 2, name: "jobaer", age: 22 };
 
-// console.log(getProperty(user2, "age"));
 
 
 // Problem 5:
@@ -60,10 +51,6 @@ const toggleReadStatus = (myBook: Book): BookWithReadStatus => {
     return { ...myBook, isRead: true };
 }
 
-const myBook = { title: "TypeScript Guide", author: "Jane Doe", publishedYear: 2024 };
-
-// const result = toggleReadStatus(myBook)
-// console.log(result)
 
 
 
@@ -90,9 +77,6 @@ class Student extends Person {
     }
 }
 
-const student1 = new Student("Imran", 20, "A+");
-
-// console.log(student1.getDetails());
 
 
 // Problem 7:
@@ -102,7 +86,4 @@ const getIntersection = (num1: number[], num2: number[]): number[] => {
     const presentInBothArrays = num1.filter(num => num2.includes(num));
     return presentInBothArrays;
 }
-
-const result = getIntersection([5, 4, 3, 2, 1], [1, 3, 8, 6, 5]);
-
-console.log(result)
+ 
