8c252af9c022bda134bcdd85de56ba37ecaf2153
imran hossain <imranme.global@gmail.com>
2026-05-07 17:27:28 +0600

    problem sloved no:7

--- solutions.ts
+++ solutions.ts
@@ -44,8 +44,7 @@ const user2 = { id: 2, name: "jobaer", age: 22 };
 // console.log(getProperty(user2, "age"));
 
 
-// Problem 4:
-
+// Problem 5:
 
 interface Book {
     title: string;
@@ -59,11 +58,53 @@ interface BookWithReadStatus extends Book {
 
 const toggleReadStatus = (myBook: Book): BookWithReadStatus => {
     return { ...myBook, isRead: true };
-
 }
 
 const myBook = { title: "TypeScript Guide", author: "Jane Doe", publishedYear: 2024 };
 
-const result = toggleReadStatus(myBook)
+// const result = toggleReadStatus(myBook)
+// console.log(result)
+
+
+
+// Problem 6:
+
+class Person {
+    name: string;
+    age: number;
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+
+class Student extends Person {
+    grade: string;
+    constructor(name: string, age: number, grade: string) {
+        super(name, age);
+        this.grade = grade;
+    }
+    getDetails(): string {
+        return `Name: ${this.name}, Age: ${this.age}, Grade: ${this.grade}`;
+    }
+}
+
+const student1 = new Student("Imran", 20, "A+");
+
+// console.log(student1.getDetails());
+
+
+// Problem 7:
+
+
+
+
+const getIntersection = (num1: number[], num2: number[]): number[] => {
+    const presentInBothArrays = num1.filter(num => num2.includes(num));
+    return presentInBothArrays;
+}
+
+const result = getIntersection([5, 4, 3, 2, 1], [1, 3, 8, 6, 5]);
 
 console.log(result)
